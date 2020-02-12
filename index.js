cc47c65e1baf6323090b27e423d8e78ac0c5e6a8
Chloe Gell <gellert.chloe@gmail.com>
2020-02-12 17:11:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Cat {
+  constructor(name, sex){
+  this.name = name;
+  this.sex = sex; 
+  }
+}
+
+class Dog {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex; 
+  }
+}
+
+class Bird{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex; 
+  }
+}
+
+class Cat 
+  attr_accessor :name, :sex 
+  def initialize(name, sex)
+    @name = name 
+    @sex = sex 
+  end 
+end 
+
